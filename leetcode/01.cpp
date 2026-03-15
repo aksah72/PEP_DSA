686b9607b2f66b04e6646c1967bb6c7deeb475a7
Aman Sah <aksah72600@gmail.com>
2026-03-15 22:13:51 +0530

    Leetcode 75

--- leetcode/01.cpp
+++ leetcode/01.cpp
@@ -367,3 +367,26 @@ public:
         return ans;
     }
 };
+
+//https://leetcode.com/problems/sort-colors/description/
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int start=0,mid=0,end=nums.size()-1;
+        while(mid<=end){
+            if(nums[mid]==0){
+                swap(nums[start],nums[mid]);
+                start++;
+                mid++;
+            }
+            else if(nums[mid]==2){
+                swap(nums[mid],nums[end]);
+                end--;
+            }
+            else{
+                mid++;
+            }
+        }
+    }
+};

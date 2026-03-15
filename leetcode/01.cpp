f099a2b77ded76ece7b256315e1ed55d92ab5449
Aman Sah <aksah72600@gmail.com>
2026-03-15 22:23:20 +0530

    Leetcode 61

--- leetcode/01.cpp
+++ leetcode/01.cpp
@@ -390,3 +390,51 @@ public:
         }
     }
 };
+
+
+//https://leetcode.com/problems/rotate-list/description/?envType=problem-list-v2&envId=two-pointers
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* solve(ListNode* head){
+        ListNode* temp = head;
+        ListNode* prev = NULL;
+        while(temp->next != NULL){
+            prev = temp;
+            temp = temp->next;
+        }
+        prev->next = NULL; 
+        temp->next = head;
+        head = temp;
+        return head; 
+    }
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head==NULL || head->next == NULL) return head;
+
+        ListNode* temp = head;
+        ListNode* curr = head;
+        int cnt = 0;
+
+        while(curr){
+            curr = curr->next;
+            cnt++;
+        }
+        while(k>cnt){
+             k = k%cnt;
+        }
+        while(k--){
+            temp = solve(temp);
+        }
+        return temp;
+    }
+};

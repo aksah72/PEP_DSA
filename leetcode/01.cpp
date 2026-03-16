047e41482e4b1872a97e4357c491ed8e180b694f
Aman Sah <aksah72600@gmail.com>
2026-03-16 11:22:21 +0530

    Leetcode 80

--- leetcode/01.cpp
+++ leetcode/01.cpp
@@ -438,3 +438,20 @@ public:
         return temp;
     }
 };
+
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/description/?envType=problem-list-v2&envId=two-pointers
+
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+        for(int num : nums){
+            if(i < 2 || num != nums[i-2]){
+                nums[i] = num;
+                i++;
+            }
+        }
+        return i;
+    }
+};

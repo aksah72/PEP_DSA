71f7e0625ad1d31fdfd7ba603197b0475578832d
Aman Sah <aksah72600@gmail.com>
2026-03-18 21:20:38 +0530

    Leetcode 905

--- leetcode/01.cpp
+++ leetcode/01.cpp
@@ -553,3 +553,23 @@ public:
         return slow;
     }
 };
+
+//https://leetcode.com/problems/sort-array-by-parity/description/?envType=problem-list-v2&envId=two-pointers
+
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int i = 0, j = nums.size() - 1;
+        
+        while(i < j){
+            if(nums[i] % 2 > nums[j] % 2){
+                swap(nums[i], nums[j]);
+            }
+            
+            if(nums[i] % 2 == 0) i++;
+            if(nums[j] % 2 == 1) j--;
+        }
+        
+        return nums;
+    }
+};

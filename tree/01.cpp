717dccb881b4b03e7fb032c733dbfc42d746d431
Aman Sah <aksah72600@gmail.com>
2026-03-19 14:47:16 +0530

    Working ok stack in tree

--- tree/01.cpp
+++ tree/01.cpp
@@ -30,10 +30,11 @@ void postorder(Node * root , vector<int> &result){
 
 void inorder(Node * root , vector<int> &result){
     if(root == NULL) return;
-    preorder(root->left,result);
-    result.push_back(root->data);
-    preorder(root->right,result);
-    
+    inorder(root->left,result);
+    cout<<root->data<<"UP"<<endl;
+    //result.push_back(root->data);
+    inorder(root->right,result);
+    cout<<root->data<<"DOWN"<<endl;
 }
 
 int countLeafNode(Node* root){
@@ -47,20 +48,26 @@ int countLeafNode(Node* root){
 
 int main(){
     Node* root = NULL;
-    root = new Node(5);
-    root->left = new Node(4);
-    root->right = new Node(3);
-    root->left->left = new Node(2);
-    root->left->right = new Node(1);
-    root->right->left = new Node(10);
-    root->right->right = new Node(20);
-    int a = countLeafNode(root);
-    cout<<a;
+    root = new Node(1);
+    //root->left = new Node(4);
+    root->right = new Node(8);
+    //root->left->left = new Node(4);
+    //root->left->right = new Node(1);
+    root->right->left = new Node(4);
+    //root->right->right = new Node(20);
+    root->right->left->left = new Node(2);
+    root->right->left->right = new Node(3);
+    root->right->left->left->right = new Node(9);
+
+
+    //int a = countLeafNode(root);
+    //cout<<a;
 
     vector<int> result;
-    preorder(root,result);
-    for(int i : result) {
-        cout<<i<<" ";
-    }
+    //preorder(root,result);
+    //for(int i : result) {
+    //    cout<<i<<" ";
+    // }
+    inorder(root,result);
 
 }

3a827961de7118afef33a1338e8a8da0d779d428
Aman Sah <aksah72600@gmail.com>
2026-03-25 20:13:39 +0530

    leetcode 101

--- .history/leetcode/01_20260325201315.cpp
+++ .history/leetcode/01_20260325201315.cpp
@@ -619,20 +619,4 @@ public:
     }
 };
   
-//https://leetcode.com/problems/symmetric-tree/description/?envType=problem-list-v2&envId=binary-tree
-
-class Solution {
-public:
-    bool isMirror(TreeNode* left, TreeNode* right){
-        if(left == NULL && right == NULL) return true;
-        if(left == NULL || right == NULL) return false;
-        if(left->val != right->val) return false;
-        return isMirror(left->left, right->right) && 
-               isMirror(left->right, right->left);
-    }
-
-    bool isSymmetric(TreeNode* root) {
-        if(root == NULL) return true;
-        return isMirror(root->left, root->right);
-    }
-};
+//

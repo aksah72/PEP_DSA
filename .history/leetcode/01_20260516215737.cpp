2c3568ed31574e19e4e454f3f963fb8888fcbebe
Aman Sah <aksah72600@gmail.com>
2026-05-16 21:57:55 +0530

    leetcode 345

--- .history/leetcode/01_20260516215737.cpp
+++ .history/leetcode/01_20260516215737.cpp
@@ -776,35 +776,7 @@ public:
 
 //https://leetcode.com/problems/reverse-vowels-of-a-string/description/?envType=problem-list-v2&envId=string
 
-class Solution {
-public:
-    bool isVowel(char c) {
-        c = tolower(c);
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
-    }
 
-    string reverseVowels(string s) {
-        int i = 0, j = s.size() - 1;
-
-        while (i < j) {
-
-            while (i < j && !isVowel(s[i])) {
-                i++;
-            }
-
-            while (i < j && !isVowel(s[j])) {
-                j--;
-            }
-
-            swap(s[i], s[j]);
-
-            i++;
-            j--;
-        }
-
-        return s;
-    }
-};
 
 
 

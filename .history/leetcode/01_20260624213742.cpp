2f5a936a69e9eaa50585abcee2feb0578ae9ceda
Aman Sah <aksah72600@gmail.com>
2026-06-24 21:37:56 +0530

    leetcode 1823

--- .history/leetcode/01_20260624213742.cpp
+++ .history/leetcode/01_20260624213742.cpp
@@ -1084,16 +1084,7 @@ public:
 
 //https://leetcode.com/problems/find-the-winner-of-the-circular-game/description/
 
-class Solution {
-public:
-    int findTheWinner(int n, int k) {
-        int ans=0;
-        for(int i=1;i<=n;i++){
-            ans = (ans+k)%i;
-        }
-        return ans+1;
-    }
-};
+
 
 
 
